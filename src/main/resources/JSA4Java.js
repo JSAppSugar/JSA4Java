2edd4af8c0bd785078f2c0a2695a1e45b27902c9
Neal <>
2018-11-01 14:03:09 +0800

    convert Java type to JS type

--- src/main/resources/JSA4Java.js
+++ src/main/resources/JSA4Java.js
@@ -5,6 +5,18 @@ var $engine = $engine || {};
 
 (function(){
 	"use strict";
+	
+	var typesMapJ2JS = {
+		"java.lang.Short" : "intValue",
+		"java.lang.Integer" : "intValue",
+		"java.lang.Character" : "",
+		"java.lang.Long" : "longValue",
+		"java.lang.Boolean" : "booleanValue",
+		"java.lang.Float" : "doubleValue",
+		"java.lang.Double" : "doubleValue",
+		"java.lang.Byte" : "intValue",
+		"java.lang.String" : "",
+	};
 
 	$engine.lang = "java";
 
@@ -17,7 +29,16 @@ var $engine = $engine || {};
 	$engine.$function = function(define){
 		var method = define;
 		return (function(){
-			return $context.invokeMethod(this.$this,method,Array.prototype.slice.call(arguments));;
+			var v = $context.invokeMethod(this.$this,method,Array.prototype.slice.call(arguments));
+			if(v["getClass"]){
+				var javaClass = v.getClass().getName();
+				var toJS = typesMapJ2JS[javaClass];
+				if(toJS != undefined){
+					if(toJS == "") v = v+"";
+					else v = v[toJS]();
+				}
+			}
+			return v;
 		});
 	}
 
