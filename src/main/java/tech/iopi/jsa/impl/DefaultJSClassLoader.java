19c8b12acf4e81bf184dc276917f7dc31e03ee79
Neal <>
2018-11-04 01:56:45 +0800

    Compatible Android

--- src/main/java/tech/iopi/jsa/impl/DefaultJSClassLoader.java
+++ src/main/java/tech/iopi/jsa/impl/DefaultJSClassLoader.java
@@ -1,8 +1,8 @@
 package tech.iopi.jsa.impl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.net.URL;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 import tech.iopi.jsa.JSClassLoader;
 
@@ -14,22 +14,48 @@ import tech.iopi.jsa.JSClassLoader;
  */
 public class DefaultJSClassLoader implements JSClassLoader {
 	
+    private static final int DEFAULT_BUFFER_SIZE = 8192;
+    private static final int MAX_BUFFER_SIZE = Integer.MAX_VALUE - 8;
+	
 	public String loadJSClass(String className) {
 		String classFilePath = className.replaceAll("\\.", "/") +".js";
-		URL url = Thread.currentThread().getContextClassLoader().getResource(classFilePath);
-		if(url!=null) {
-			File file = new File(url.getFile());
-			Long filelength = file.length();
-			byte[] filecontent = new byte[filelength.intValue()];
-			try {
-				FileInputStream in = new FileInputStream(file);  
-	            in.read(filecontent);  
-	            in.close();
+		InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(classFilePath);
+		if(in!=null) {
+			try { 
+				byte[] filecontent = readAllBytes(in);
 	            return new String(filecontent, "UTF-8");
 			}catch(Exception e) {
-				
+				throw new RuntimeException(e);
+			}finally {
+				try {
+					in.close();
+				} catch (IOException e) {
+					
+				}
 			}
 		}
 		return null;
+	}
+	
+	private static byte[] readAllBytes(InputStream in) throws IOException {
+		byte[] buf = new byte[DEFAULT_BUFFER_SIZE];
+		int capacity = buf.length;
+		int nread = 0;
+		int n;
+		for (;;) {
+			while ((n = in.read(buf, nread, capacity - nread)) > 0)
+				nread += n;
+			if (n < 0)
+				break;
+			if (capacity <= MAX_BUFFER_SIZE - capacity) {
+				capacity = capacity << 1;
+			} else {
+				if (capacity == MAX_BUFFER_SIZE)
+					throw new OutOfMemoryError("Required array size too large");
+				capacity = MAX_BUFFER_SIZE;
+			}
+			buf = Arrays.copyOf(buf, capacity);
+		}
+		return (capacity == nread) ? buf : Arrays.copyOf(buf, nread);
 	}
 }
